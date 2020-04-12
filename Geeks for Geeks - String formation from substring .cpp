64ccd6fc88eb67946a70c305934d0a62ad528fb1
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2020-04-12 10:16:12 -0700

    Create Geeks for Geeks - String formation from substring .cpp

---
+++ Geeks for Geeks - String formation from substring .cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char pat[100001];
+int lps[100001];
+/// KMP pattern Matching O (n+m) time complexity ,where n and m is the length of text and pattern length
+/// Here ,we use the logic of longest suffix which is also prefix
+
+
+/// Fills lps[] for given patttern pat[0..M-1]
+/// A two pointer approach
+int computeLPSArray()
+{
+    /// traverser of prefix ,left
+    int left = 0;
+    int P=(strlen)(pat);
+    lps[0] = 0; /// lps[0] is always 0
+
+    int right = 1;
+    while (right < P) {
+        if (pat[right] == pat[left]) {
+            left++;
+            lps[right] = left;/// take longest suffix length which is also prefix
+            right++;
+        }
+        else /// (pat[right] != pat[left])
+        {
+            if (left != 0) {
+                left = lps[left - 1];
+            }
+            else /// if (len == 0) ,then start from again start ,because there is no common suffix and prefix till right index
+            {
+                lps[right] = 0;
+                right++;
+            }
+        }
+    }
+    return lps[P-1];
+}
+
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
+    scanf("%s",pat);
+    int n=strlen(pat);
+    int ans=computeLPSArray();
+    if((n+1)/2<=ans)
+    printf("True\n");/// That means string consists of same substring 
+    else
+    printf("False\n");
+    }
+    return 0;
+}
