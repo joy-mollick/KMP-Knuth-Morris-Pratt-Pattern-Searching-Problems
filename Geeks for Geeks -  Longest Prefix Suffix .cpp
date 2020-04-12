6020f26829db7d34559ca7e4970e34be548807cd
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2020-04-12 09:52:39 -0700

    Create Geeks for Geeks -  Longest Prefix Suffix .cpp

---
+++ Geeks for Geeks -  Longest Prefix Suffix .cpp
@@ -0,0 +1,54 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char pat[100001];
+int lps[100001];
+/// KMP pattern Matching O (n+m) time complexity ,where n and m is the length of text and pattern length
+/// Here ,we use the logic of longest suffix which is also prefix
+
+
+/// Fills lps[] for given patttern pat[0..M-1]
+/// A two pointer approach
+int computeLPSArray()
+{
+    /// traverser of prefix ,left
+    int left = 0;
+    int P=strlen(pat);
+    lps[0] = 0; /// lps[0] is always 0
+
+    int right = 1;
+    while (right < P) {
+        if (pat[right] == pat[left]) {
+            left++;
+            lps[right] = left;/// take longest suffix length which is also prefix
+            right++;
+        }
+        else /// (pat[right] != pat[left])
+        {
+            if (left != 0) {
+                left = lps[left - 1];
+            }
+            else /// if (len == 0) ,then start from again start ,because there is no common suffix and prefix till right index
+            {
+                lps[right] = 0;
+                right++;
+            }
+        }
+    }
+    return lps[P-1];
+}
+
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
+    scanf("%s",pat);
+    int ans=computeLPSArray();
+    printf("%d\n",ans);
+    }
+    return 0;
+}
